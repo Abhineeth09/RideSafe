8ef22676f66b8ccfe169da7cf4fb612ad321a862
justin colyar <=>
2021-04-09 14:45:13 -0700

    Added initial frame for REST Service

---
+++ Location Service/Location Service/App_Code/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	[WebGet(UriTemplate = "CheckLocation?startLong={start_longitude}&startLat={start_latitude}&endLong={end_longitude}&endLat={end_latitude}&userID={userID}")]
+	string CheckLocation(int start_longitude, int start_latitude, int end_longitude, int end_latitude, int userID);
+
+}
